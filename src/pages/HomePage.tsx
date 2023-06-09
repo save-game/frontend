5468824b72cff5553061d3dad4f3feb557e8513d
SamLee <goha2010@naver.com>
2023-06-09 18:17:18 +0900

    Home 페이지에 그래프 추가. 그래프를 그리기 위해서 임시로 test 데이터를 추가함.(추후 삭제 예정)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,37 @@
+import { useState } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+import ExpenseGraph from "../components/ExpenseGraph";
+
 export default function Home() {
-  return <></>;
+  const [imgSrc, setImgSrc] = useState("https://picsum.photos/70");
+  const [nickName, setNickName] = useState("닉네임(기본값)");
+
+  return (
+    <>
+      <Container>
+        <ProfileContainer>
+          <ProfileImg src={imgSrc} />
+          <p className="ml-4 text-accent text-lg">{nickName}</p>
+        </ProfileContainer>
+        <ExpenseGraph />
+      </Container>
+    </>
+  );
 }
+
+const Container = styled.div`
+  ${tw`mx-auto w-11/12 pt-8 text-neutral-500 font-bold text-sm flex flex-col justify-center items-center`}
+`;
+
+const ProfileContainer = styled.div`
+  font-weight: 300;
+  ${tw`h-20 w-full flex items-center mt-10 px-4 pb-2 shadow-md rounded-md`}
+`;
+
+const ProfileImg = styled.img`
+  width: 70px;
+  height: 70px;
+  ${tw` rounded-full`};
+`;
