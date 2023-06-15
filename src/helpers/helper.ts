a8327a744b2bbcd60dc0df4317e51d4d3b14a531
JeongSeonP <finchblue@naver.com>
2023-06-15 23:51:50 +0900

    :memo: Helper파일에 날짜범위구하는 함수 추가

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -6,3 +6,18 @@ export const dDayCalculator = (date: string) => {
 
   return daysDiff;
 };
+
+export const dateRangeCalculator = (start: string, end: string) => {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+  const timeDiff = endDate.getTime() - startDate.getTime();
+  const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24)) + 1;
+
+  const dateRange = [];
+  for (let i = 0; i < daysDiff; i++) {
+    const date = new Date(startDate);
+    date.setDate(startDate.getDate() + i);
+    dateRange.push(date.toLocaleDateString("en-US"));
+  }
+  return dateRange;
+};
