94be79385d82637c5b14a7bc5ec737bebd1f801e
SamLee <goha2010@naver.com>
2023-06-23 19:52:21 +0900

    Challenge 등록을 위한 Category 추가. 기존의 Category에 전체(all) 추가함.

---
+++ src/components/Challenge/ChallengeCategoryFilter.tsx
@@ -0,0 +1,36 @@
+import { Control, Controller, FieldValues } from "react-hook-form";
+
+import {
+  Category,
+  ChanllegeCategoryList,
+} from "../../constants/expenseCategory";
+
+export default function ChallengeCategoryFilter({
+  control,
+  handleSelectCategory,
+}: {
+  control: Control<FieldValues>;
+  handleSelectCategory: (item: Category) => void;
+}) {
+  return (
+    <>
+      <div className="w-full grid grid-cols-5">
+        {ChanllegeCategoryList.map((item) => (
+          <button
+            key={item.category}
+            type="button"
+            onClick={() => handleSelectCategory(item)}
+            className="btn btn-ghost focus:border focus:bg-zinc-300 hover:bg-transparent w-full p-0 block col-span-1 h-20"
+          >
+            <div
+              className={`${item.color} w-4/5 mx-auto text-base-100 mb-1 flex justify-center items-center h-10 rounded-full`}
+            >
+              {item.icon}
+            </div>
+            <div className="text-xs font-normal">{item.name}</div>
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
