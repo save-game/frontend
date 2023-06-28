1f6429df9693c2e1476fae15b937ec7b4dab58d2
SamLee <goha2010@naver.com>
2023-06-28 22:50:43 +0900

    사용하지 않는 메서드 삭제

--- src/components/Expenses/ExpenseGraphContainer.tsx
+++ src/components/Expenses/ExpenseGraphContainer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 import ExpenseGraph from "./ExpenseGraph";
 import { getRecordedeExpenseForAnalyze } from "../../api/expenseAPI";
