4cdfcc9af23c8d27dcc1201cb0f08cb78db38de1
bidanee <dleksql119@gmail.com>
2023-07-05 12:32:29 +0900

    Img 업로드 관련 firebaseAPI

--- src/components/Board/ImgUpload.tsx
+++ src/components/Board/ImgUpload.tsx
@@ -1,11 +1,11 @@
-import { ChangeEvent, useRef } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import { TiPlus } from "react-icons/ti";
 import { MdPhotoCamera } from "react-icons/Md";
 import { SHOW_WARNING_MODAL_DELAY } from "../../constants/modalTime";
 import { useRecoilState } from "recoil";
 import { showImgState } from "../../Recoil/boardAtom";
 import imageCompression from "browser-image-compression";
-import { getBoardUrl } from "../../api/firebaseAPI";
+import { getBoardUrl, uploadCompressedImages } from "../../api/firebaseAPI";
 import { useParams } from "react-router-dom";
 
 export default function ImgUpLoad() {
@@ -14,6 +14,43 @@ export default function ImgUpLoad() {
   const dialogRef = useRef<HTMLDialogElement>(null);
   const { challengeId } = useParams();
 
+  //속도...
+  // const [thumbnail, setTumbnail] = useState<string[]>([]);
+  // const [imgFileLists, setImgFileLists] = useState<File[]>([]);
+
+  // const handleImg = (e: ChangeEvent<HTMLInputElement>) => {
+  //   if (!e.target.files) return;
+  //   const file = e.target.files[0];
+  //   let imgFiles: string[] = [...thumbnail];
+  //   let imgLists: File[] = [...imgFileLists];
+  //   const url = URL.createObjectURL(file);
+  //   imgFiles.push(url);
+  //   imgLists.push(file);
+
+  //   if (imgFiles.length > 3 && imgLists.length > 3) {
+  //     imgFiles = imgFiles.slice(0, 3);
+  //     imgLists = imgLists.slice(0, 3);
+  //     if (dialogRef.current) {
+  //       dialogRef.current?.showModal();
+  //     } else {
+  //       return;
+  //     }
+  //     setTimeout(() => {
+  //       if (dialogRef.current) {
+  //         dialogRef.current.close();
+  //       }
+  //     }, SHOW_WARNING_MODAL_DELAY);
+  //     return;
+  //   }
+  //   setTumbnail(imgFiles);
+  //   setImgFileLists(imgLists);
+  //   console.log(uploadCompressedImages(imgLists, `${challengeId}/images`));
+  //   e.target.value = "";
+  // };
+  // const handleDeleteImg = (id: number) => {
+  //   setTumbnail(thumbnail.filter((_, index) => index !== id));
+  // };
+
   const uploadImg = async (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
     const file = e.target.files[0];
@@ -21,6 +58,7 @@ export default function ImgUpLoad() {
     const imgUrlLists: string[] = [...showImage];
     if (!challengeId) return;
     const boardUrl = await getBoardUrl(challengeId, reSize, Date.now());
+    console.log(boardUrl, "hi");
     boardUrl ? imgUrlLists.push(boardUrl) : null;
     setShowImage(imgUrlLists);
     if (imgUrlLists.length >= 3) {
@@ -49,6 +87,7 @@ export default function ImgUpLoad() {
         type="file"
         id="photo"
         ref={fileInputRef}
+        // onChange={handleImg}
         onChange={uploadImg}
         className="hidden"
         placeholder="hi"
@@ -68,6 +107,9 @@ export default function ImgUpLoad() {
           <span className="absolute top-2/3 text-xs left-1/3 right-1/3">
             {showImage.length}/3
           </span>
+          {/* <span className="absolute top-2/3 text-xs left-1/3 right-1/3">
+            {thumbnail.length}/3
+          </span> */}
         </label>
 
         {showImage.map((image, id) => (
@@ -82,6 +124,18 @@ export default function ImgUpLoad() {
             </button>
           </div>
         ))}
+        {/* {thumbnail.map((image, id) => (
+          <div className="w-20 h-20 border relative ml-1 mr-1" key={id}>
+            <img src={image} alt={`${image} - ${id}`} className="p-2" />
+            <button
+              type="button"
+              onClick={() => handleDeleteImg(id)}
+              className="absolute top-1 right-1 hover:text-lg"
+            >
+              X
+            </button>
+          </div>
+        ))} */}
         <dialog
           ref={dialogRef}
           className="w-10/12 py-20 rounded-lg bg-light-color shadow-lg text-neutral-600 outline-none z-[999]"
